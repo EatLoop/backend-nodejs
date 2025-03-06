7ef2cc3129c7e50c3d920b863721b2aa7f7041c8
AyushDocs <ayushalokdubey@gmail.com>
2025-03-06 10:52:01 +0530

    express app added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 /** @format */
 import configure from './config';
-
+import express from 'express'
 const main = async () => {
 	await configure();
-	
+	const app = express();
+	const port = process.env.PORT ?? 3000;
+
+	// Middleware
+	app.use(express.json());
+
+	app.get('/', (req, res) => {
+		res.send('Hello, TypeScript with Express!');
+	});
+
+	// Start server
+	app.listen(port, () => {
+		console.log(`Server running on http://localhost:${port}`);
+	});
+
 };
 
 main();
